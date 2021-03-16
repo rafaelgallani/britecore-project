5266a4b5e2d30f699ea249b065e650c68e27fd63
Rafael Galani <galani.rafael@gmail.com>
2021-03-15 21:37:09 -0300

    chore: simple about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,7 +3,7 @@
     <h1>Made by <b>Rafael Galani.</b></h1>
     <p>An insurance system PoC designed for BriteCore.</p>
     <p><b>Backend</b>: Python (uWSGI, Django, Django Rest Framework) + PostgreSQL + NGINX</p>
-    <p><b>Frontend</b>: Vue.js</p>
+    <p><b>Frontend</b>: Vue.js + NGINX</p>
     <p>This instance is hosted in AWS using ECS. All the development was containerized, so cloud deployment is seamless.</p>
     <p>Check the <a href="https://github.com/rafaelgalani/britecore-project" target="_blank" rel="noopener">repository</a> for more details.</p>
     <p>Follow me: </p>
